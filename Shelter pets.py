4325c311c3a42e66a9bcbbd54eab9a30633578d0
mhmdjawad1 <baghdadijawad74@gmail.com>
2025-07-01 05:20:07 -0700

    Updates and fixes

--- Shelter pets.py
+++ Shelter pets.py
@@ -1,5 +1,5 @@
 class pet:
-    def __init__(self,name,species,age,):
+    def __init__(self,name,species,age):
         self.name=name
         self.species=species
         self.age=age
@@ -7,7 +7,8 @@ class pet:
 
 
     def display_info(self):
-        print("Pet name is", self.name," and age is",self.age,"and its a",self.species,"and its",self.adopted)
+        current_status = "adopted" if self.adopted else "not adopted"
+        print( self.name," is",self.age,"years old and its a",self.species,"and its currently",current_status)
 
     def mark_adopted(self):
         self.adopted=True
@@ -15,11 +16,26 @@ class pet:
     def birthday(self):
         self.age+=1
 
-p1=pet("dax","cat",int("1"))
 
-p2=pet("dex","dog",int("2"))
+pet1=pet("Max","cat",int("1"))
 
-p3=pet("max","cat",int("3"))
+pet2=pet("Dex","dog",int("2"))
+
+pet3=pet("Leo","cat",int("3"))
+
+pet1.display_info()
+pet2.display_info()
+pet3.display_info()
+
+
+pet1.birthday()
+pet2.mark_adopted()
+
+
+
+pet1.display_info()
+pet2.display_info()
+pet3.display_info()
 
 
 
