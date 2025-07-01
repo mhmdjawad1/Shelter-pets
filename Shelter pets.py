10c7b33b22e1c79625228e0cf1ec57109836e89a
mhmdjawad1 <baghdadijawad74@gmail.com>
2025-07-01 04:40:55 -0700

    Set adopted to true

--- Shelter pets.py
+++ Shelter pets.py
@@ -1,9 +1,22 @@
 class pet:
-    def __init__(self,name,species,age,adopted):
+    def __init__(self,name,species,age,):
         self.name=name
         self.species=species
         self.age=age
         self.adopted=False
+
+
     def display_info(self):
-        print("Pet name is", self.name," and age is",self.age,"and its a",self.species)
+        print("Pet name is", self.name," and age is",self.age,"and its a",self.species,"and its",self.adopted)
+
+    def mark_adopted(self):
+        self.adopted=True
+
+
+    
+
+
+
+
+
     
